8719bcad401c4c604b1a6e8d57725e5028417799
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-07-22 14:27:28 +0200

    No variable need

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,7 @@
 import { KanbanInput } from './components/kanban-input.ts';
 import { TaskList } from './components/task-list.ts';
 
-const kanbanInput = new KanbanInput();
-const todo = new TaskList('to-do');
-const inprogress = new TaskList('in-progress');
-const done = new TaskList('done');
+new KanbanInput();
+new TaskList('to-do');
+new TaskList('in-progress');
+new TaskList('done');
