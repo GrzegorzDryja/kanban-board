9cb251c5d1a2348ca2d60b3cc2fa5764af1cfc66
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-07-24 12:07:46 +0200

    Decorator
    Decorator to .bind(this)

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,45 @@
 "use strict";
-var InputForm = (function () {
-    function InputForm() {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+class InputForm {
+    constructor() {
         this.templateElement = document.querySelector("#input-form");
         this.targetElement = document.querySelector("#target");
-        var importedNode = document.importNode(this.templateElement.content, true);
+        const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.element.id = "user-input";
         this.taskInput = this.element.querySelector("#task");
         this.configure();
         this.attach();
     }
-    InputForm.prototype.submitHandler = function (event) {
+    submitHandler(event) {
         event.preventDefault();
         console.log(this.taskInput.value);
-    };
-    InputForm.prototype.configure = function () {
+    }
+    configure() {
         this.element.addEventListener("submit", this.submitHandler.bind(this));
-    };
-    InputForm.prototype.attach = function () {
+    }
+    attach() {
         this.targetElement.insertAdjacentElement("afterbegin", this.element);
-    };
-    return InputForm;
-}());
-var task = new InputForm();
+    }
+}
+__decorate([
+    autobind
+], InputForm.prototype, "submitHandler", null);
+const task = new InputForm();
 //# sourceMappingURL=app.js.map
