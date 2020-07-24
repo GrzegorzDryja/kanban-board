f7d305101fb574230979694150b08d6a6456a8da
GrzegorzDryja <GrzegorzDryja.via@gmail.com>
2020-07-24 10:39:40 +0200

    addEventListner
    Interacting with DOM Elements

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,17 @@ var InputForm = (function () {
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.element.id = "user-input";
+        this.taskInput = this.element.querySelector("#task");
+        this.configure();
         this.attach();
     }
+    InputForm.prototype.submitHandler = function (event) {
+        event.preventDefault();
+        console.log(this.taskInput.value);
+    };
+    InputForm.prototype.configure = function () {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
+    };
     InputForm.prototype.attach = function () {
         this.targetElement.insertAdjacentElement("afterbegin", this.element);
     };
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ class InputForm{
   templateElement: HTMLTemplateElement;
   targetElement: HTMLDivElement;
   element: HTMLFormElement;
+  taskInput: HTMLInputElement;
 
   constructor(){
     this.templateElement = <HTMLTemplateElement> document.querySelector("#input-form");
@@ -10,7 +11,20 @@ class InputForm{
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = <HTMLFormElement> importedNode.firstElementChild;
     this.element.id = "user-input";
+
+    this.taskInput = <HTMLInputElement> this.element.querySelector("#task");
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.taskInput.value)
+  }
+
+  private configure(){
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
